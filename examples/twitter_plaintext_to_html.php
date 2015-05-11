915f6e149b72b0e831f872ab6cd95ef0c4ab4a61
Bobby Allen <ballen@bobbyallen.me>
2015-05-11 13:50:47 +0100

    Updated example text.

--- examples/twitter_plaintext_to_html.php
+++ examples/twitter_plaintext_to_html.php
@@ -11,5 +11,5 @@ echo "<p>The example Twitter string (in realworld scenario, this would probably 
 echo "This is a plain-text version example of the tweet that we have pulled from the API or database:";
 echo "<p><pre>{$tweet}</pre></p>";
 
-echo "Using the ->html() on the string will, by default will redender is for use with Twitter... ";
+echo "Using the ->html() on the string will, by default will render is for use with Twitter... ";
 echo "<p><pre>{$conversion->html()}</pre></p>";
