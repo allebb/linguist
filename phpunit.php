0402118c0b0cf5c7afae09a9f91056f4cb7d3ef5
Bobby Allen <ballen@bobbyallen.me>
2015-05-17 18:37:46 +0100

    Adding phpunit to composer and added base test class.

---
+++ phpunit.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Linguist
+ *
+ * Linguist is a PHP library for parsing strings, it can extract and manipulate
+ *  prefixed words in content ideal for working with @mentions, #topics and
+ *  even custom action tags!
+ *
+ * @author Bobby Allen <ballen@bobbyallen.me>
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-3.0.html
+ * @link https://github.com/bobsta63/linguist
+ * @link http://www.bobbyallen.me
+ *
+ */
+/**
+ * PHPUNIT BOOTSTRAP FILE USED TO LOAD THE COMPOSER AUTOLOADER SO WE CAN
+ * UTILISE THE PACKAGE CLASSES IN THE TEST CLASSES.
+ */
+require __DIR__ . '/vendor/autoload.php';
---
+++ tests/ParserTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Distical
+ *
+ * Distical is a simple distance calculator library for PHP 5.3+ which
+ * amongst other things can calculate the distance between two or more lat/long
+ * co-ordinates.
+ *
+ * @author Bobby Allen <ballen@bobbyallen.me>
+ * @version 2.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/distical
+ * @link http://www.bobbyallen.me
+ *
+ */
+use Ballen\Linguist\Parser as TagParser;
+use Ballen\Linguist\Configuration as TagConfiguration;
+use \PHPUnit_Framework_TestCase;
+
+class ParserTest extends PHPUnit_Framework_TestCase
+{
+
+    private $example_twitter = "Hey @bobsta63, this example is for parsing plain-text tweet strings into HTML right? #questions #howto";
+
+    public function __construct()
+    {
+        
+    }
+}
