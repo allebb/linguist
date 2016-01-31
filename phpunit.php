18a5695c5361ad5d1f703eaeff25bf39119d8a27
Bobby Allen <ballen@bobbyallen.me>
2016-01-31 17:44:21 +0000

    Removal of acceptence of PHP7 test failures (now that PHP7 is now GA)

--- phpunit.php
+++ phpunit.php
@@ -7,7 +7,6 @@
  *  even custom action tags!
  *
  * @author Bobby Allen <ballen@bobbyallen.me>
- * @version 1.0.0
  * @license http://www.gnu.org/licenses/gpl-3.0.html
  * @link https://github.com/bobsta63/linguist
  * @link http://www.bobbyallen.me
